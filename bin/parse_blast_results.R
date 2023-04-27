8e5b4ee82804a973818498b4573775a18388809c
Vladimir Mikryukov <v.mikryukov@gmail.com>
2023-04-27 18:05:21 +0300

    `parse_blast_results` - main part

--- bin/parse_blast_results.R
+++ bin/parse_blast_results.R
@@ -39,3 +39,63 @@ cat(paste("Number of CPU threads to use: ",  CPUTHREADS, "\n", sep=""))
 
 cat("\n")
 
+############################################## Load packages and data
+
+cat("Loading R packages...\n")
+
+load_pckg <- function(pkg = "data.table"){
+  suppressPackageStartupMessages( library(package = pkg, character.only = TRUE) )
+  cat(paste(pkg, packageVersion(pkg), "\n"))
+}
+
+load_pckg("data.table")
+load_pckg("plyr")
+load_pckg("metagMisc")
+load_pckg("Biostrings")
+load_pckg("openxlsx")
+
+cat("\n")
+
+## Set CPU thread number
+cat("Setting number of CPU threads to: ", CPUTHREADS, "\n")
+setDTthreads(threads = CPUTHREADS)  # for data.table
+
+
+##### Parse BLAST
+
+## Load sequences
+cat("Loading FASTA sequneces\n")
+seqs <- FASTA
+seqs <- readDNAStringSet(seqs, format="fasta")
+seqs_names <- names(seqs)
+names(seqs) <- gsub(pattern = ";.*", replacement = "", x = names(seqs))
+
+## Load reference sequences
+cat("Loading BLAST database\n")
+db_refs <- DATABASE
+db_refs <- readDNAStringSet(db_refs, format="fasta")
+## BLAST columns
+bcolz <- c(
+  "QueryName", "TargetName",
+  "SeqIdentity", "AlignLen", "MismatchN", "GapOpenings",
+  "QueryStart", "QueryEnd", "TargetStart", "TargetEnd", "Evalue", "BitScore")
+
+## Load blast results
+cat("Reading m8 file\n")
+BLASTS_10h <- read_m8(INPUT, blast_colz = bcolz, package = "data.table")
+
+## Remove size annotations
+cat("Extracting query IDs\n")
+BLASTS_10h[ , QueryName := tstrsplit(QueryName, ";", keep=1) ]
+  cat("Splitting taxonomy string\n")
+  BLASTS_10h[, c("AccID", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "Function", "LT_comment", "SH") := tstrsplit(TargetName, ";", keep=1:11) ]
+
+  tcolz <- c("AccID", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "Function", "LT_comment", "SH")
+  bcolz <- c(bcolz[-2], tcolz)
+  BLASTS_10h <- BLASTS_10h[, ..bcolz]
+
+  ## Convert to wide format
+  cat("Converting to wide format\n")
+  BW <- blast_to_wide(BLASTS_10h, max_hits = 10,
+    taxonomy = c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "Function", "LT_comment", "SH"),
+    seqs = seqs, refs = db_refs)
